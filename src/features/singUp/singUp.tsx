8251ab23072cef993132793c6bedc3a1887b5c37
Tsyriulnikov <max_djvu@mail.ru>
2022-07-23 23:25:52 +0300

    signUp 3

--- src/features/singUp/singUp.tsx
+++ src/features/singUp/singUp.tsx
@@ -3,6 +3,7 @@ import {useDispatch} from "react-redux";
 import {setNewUserTC} from "./signUp-reducer";
 import {useForm} from "react-hook-form";
 import {Button, TextField} from "@material-ui/core";
+import {useNavigate} from "react-router-dom";
 
 
 type FormData = {
@@ -18,33 +19,58 @@ export const SingUp = () => {
         dispatch(setNewUserTC('1max@mail.com', 'mypassword') as any)
     }
 
-    const {register, handleSubmit, formState: {errors}} = useForm<FormData>({
+    const {register, handleSubmit, getValues, reset, formState: {errors, isValid}} = useForm<FormData>({
         defaultValues: {
             email: '',
             password: '',
             confirmPassword: '',
-
-        }
+        },
+        mode: "onBlur",
     });
     const onSubmit = (data: FormData) => {
-        console.log(data)
         dispatch(setNewUserTC(data.email, data.password) as any)
+        reset()
     };
-
+    const cancelHandler = () => {
+        reset()
+    }
 
     return (
-        // <div>
-        //     SING UP
-        // </div>
-
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("email")} placeholder="Email"/>
+            <input {...register("email", {
+                required: 'Required!',
+                pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            })} placeholder="Email"/>
+            <div style={{height: 40}}>{errors?.email && <p>{errors?.email?.message || 'Error!'}</p>}</div>
 
-            <input {...register("password")} placeholder="Password"/>
+            <input {...register("password", {required: "Password is required!"})} placeholder="Password"/>
+            {errors.password && (
+                <p style={{color: "red"}}>{errors.password.message}</p>
+            )}
 
-            <input {...register("confirmPassword")} placeholder="Confirm password"/>
+            <input {...register("confirmPassword", {
+                required: "Please confirm password!",
+                validate: {
+                    matchesPreviousPassword: (value) => {
+                        const {password} = getValues();
+                        return password === value || "Passwords should match!";
+                    }
+                }
+            })} placeholder="Confirm password"/>
+            {errors.confirmPassword && (
+                <p style={{color: "red"}}>
+                    {errors.confirmPassword.message}
+                </p>
+            )}
 
-            <input type="submit"/>
+            <input type="submit" disabled={!isValid}/>
+
+            <input
+                style={{display: "block", marginTop: 20}}
+                type="button"
+                onClick={cancelHandler}
+                value="Cancel"
+            />
         </form>
     );
 };
