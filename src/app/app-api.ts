cb07a270cd01be204e88eb6296bd85b77e4782e2
Tsyriulnikov <max_djvu@mail.ru>
2022-07-26 09:40:36 +0300

    26_07_22 1

--- src/app/app-api.ts
+++
@@ -1,7 +0,0 @@
-export type ResponseType<D = {}> = {
-    resultCode: number
-    messages: Array<string>
-    fieldsErrors: Array<string>
-    data: D
-}
-
--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -3,7 +3,7 @@ import {profileAPI} from "../features/profile/profile-api";
 
 
 const initialState: InitialStateType = {
-    status: 'idle',
+    // status: 'idle',
     error: null,
     isInitialized: false,
     isLoggedIn: false
@@ -20,8 +20,8 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
             return {...state, status: action.status}
         case 'APP/SET-ERROR':
             return {...state, error: action.error}
-        case "APP/SET-INITIALIZED":
-            return {...state, isInitialized: action.isInitialized}
+        // case "APP/SET-INITIALIZED":
+        //     return {...state, isInitialized: action.isInitialized}
 
         default:
             return state
@@ -39,16 +39,16 @@ export const setIsLoggedInAC = (value: boolean) => {
 //AC
 export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
 export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
-export const setInitializedAC = (isInitialized: boolean) => ({type: 'APP/SET-INITIALIZED', isInitialized} as const)
+// export const setInitializedAC = (isInitialized: boolean) => ({type: 'APP/SET-INITIALIZED', isInitialized} as const)
 
 export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
 export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
-export type SetInitializedActionType = ReturnType<typeof setInitializedAC>
+// export type SetInitializedActionType = ReturnType<typeof setInitializedAC>
 
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 export type InitialStateType = {
     // происходит ли сейчас взаимодействие с сервером
-    status: RequestStatusType
+    // status: RequestStatusType
     // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
     error: string | null
     isInitialized: boolean
@@ -70,5 +70,5 @@ type ActionsType =
     | ReturnType<typeof setIsLoggedInAC>
     | SetAppErrorActionType
     | SetAppStatusActionType
-    | SetInitializedActionType
+    // | SetInitializedActionType
 
