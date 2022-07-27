7f3bc6c13affe44ffaacd1b6930f4eeab3658cba
Tsyriulnikov <max_djvu@mail.ru>
2022-07-27 08:54:56 +0300

    27_07_22 3

--- src/features/singUp/api-signUp.ts
+++ src/features/singUp/api-signUp.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 const instance = axios.create({
     withCredentials: true,
     // baseURL: 'http://localhost:7542/2.0/',
@@ -11,17 +10,6 @@ export const signUpApi = {
 
     registration(email: string, password: string) {
         return instance.post('auth/register', {email, password})
-
     }
 }
 
-
-
-
-// export const getTimeAPI = (time: string) => {
-//     return instance.get(`ping?frontTime=${time}`)
-// }
-
-
-//types
-// export type nameType = {}
