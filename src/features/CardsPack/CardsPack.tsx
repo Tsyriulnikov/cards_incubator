377c86d8082a1fa5db29af62d7f2a253450cae4c
Tsyriulnikov <max_djvu@mail.ru>
2022-07-31 10:52:33 +0300

    31_07_22 1

--- src/features/CardsPack/CardsPack.tsx
+++ src/features/CardsPack/CardsPack.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { useDemoData } from '@mui/x-data-grid-generator';
 
 export const CardsPack = () => {
+    const { data } = useDemoData({
+        dataSet: 'Commodity',
+        rowLength: 5,
+        maxColumns: 6,
+    });
+
     return (
-        <div>
-            CARDS
+        <div style={{ height: 400, width: '100%', background:'white'}}>
+            <div style={{ display: 'flex', height: '100%' }}>
+                <div style={{ flexGrow: 1 }}>
+                    <DataGrid {...data} />
+                </div>
+            </div>
         </div>
     );
-};
+}
 
