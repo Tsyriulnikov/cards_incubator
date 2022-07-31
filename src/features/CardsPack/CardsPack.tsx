fdc541c32cc254363c794a523720ebd91c3616e6
Tsyriulnikov <max_djvu@mail.ru>
2022-07-31 11:06:32 +0300

    31_07_22 2

--- src/features/CardsPack/CardsPack.tsx
+++ src/features/CardsPack/CardsPack.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { useDemoData } from '@mui/x-data-grid-generator';
+import {PacksList} from "./packsList/packsList";
 
 export const CardsPack = () => {
-    const { data } = useDemoData({
-        dataSet: 'Commodity',
-        rowLength: 5,
-        maxColumns: 6,
-    });
 
     return (
-        <div style={{ height: 400, width: '100%', background:'white'}}>
-            <div style={{ display: 'flex', height: '100%' }}>
-                <div style={{ flexGrow: 1 }}>
-                    <DataGrid {...data} />
-                </div>
-            </div>
+        <div style={{ height: '100%', width: '100%'}}>
+          <PacksList/>
         </div>
     );
 }
---
+++ src/features/CardsPack/packsList/packsList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { useDemoData } from '@mui/x-data-grid-generator';
+
+export const PacksList = () => {
+    const { data } = useDemoData({
+        dataSet: 'Commodity',
+        rowLength: 5,
+        maxColumns: 6,
+    });
+
+    return (
+        <div style={{ height: 400, width: '100%', background:'white'}}>
+            <div style={{ display: 'flex', height: '100%' }}>
+                <div style={{ flexGrow: 1 }}>
+                    <DataGrid {...data} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
